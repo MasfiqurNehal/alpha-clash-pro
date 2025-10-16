bed778a2e2d37b1233ea6795519a5920831a31ed
MasfiqurNehal <masfiqur.nehal509@gmail.com>
2025-10-17 01:06:41 +0600

    created home-scrren, playground-screen, score-screen and simple script file

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,7 @@
+function play() {
+  const homeSection = document.getElementById("home-screen");
+  homeSection.classList.add("hidden");
+
+  const playGroundSection = document.getElementById("play-ground");
+  playGroundSection.classList.remove("hidden");
+}
